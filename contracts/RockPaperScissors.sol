816cc6caedae87039926f4a874649e70e895ac16
Luke Ohba <lukeohba@gmail.com>
2021-05-28 20:51:04 -0700

    added functions

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -1,40 +1,53 @@
 pragma solidity 0.8.0;
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-
-contract RockPaperScissors {
+import "@openzeppelinV2/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
+contract RockPaperScissors  is Ownable{
     using SafeMath for uint256;
-    constructor() public {
-
+    using SafeERC20 for IERC20;
+    
+    IERC20 public token; 
+    uint fee; // entrance fee
+    
+    constructor(IERC20 _token, uint _fee) {
+        token = _token;
+        fee = _fee;
     }
-
-    uint8 constant ROCK = 0;
-    uint8 constant PAPER = 1;
-    uint8 constant SCISSORS = 2;
-
-    mapping(address => uint8) public choices;
-
-    function enter() public payable {
-        require(msg.value >= getEntranceFee(), "Not enough tokens to play");
+    
+    struct Round {
+         address alice;
+         address bob;
+         mapping(address => uint256) moves;
+     }
+    
+    
+    //maps round ids to user moves. 
+    mapping(uint256 => uint8) private firstChoice;
+    mapping(uint256 => address) private gameStarter;
+    mapping(uint256 => Round) internal roundData;
+    
+    //0 = ROCK 1 = PAPER 2 = SCISSORS
+    enum RockPaperScissors{ ROCK, PAPER, SCISSORS }
+    
+    mapping(address => uint8) public choices; // keeps track of player choices
+    
+    struct Player {
+        uint playerNumber;
+    }
+    
+    //Create new round with id and entrance fee
+    function createRound(uint256 _roundId, uint256 _fee) external {
+        Round storage round = roundData[_roundId];
+    }
+    
+    function releaseFunds() public onlyOwner {
         
-
     }
-
-    function getEntranceFee() public view returns (uint256) {
-        
-
+    
+    
+    function startGame(uint8 choice) public {
+        //require(choice == ROCK || choice == PAPER || choice == SCISSORS);
+        require(choices[msg.sender] == 0);
+        choices[msg.sender] = choice;
     }
-
-    function startBattle(uint8 choice) public {
-        require(choice == ROCK || choice == PAPER || choice == SCISSORS);
-        
-    }
-
-    function endBattle() public {
-
-    }
-
-    function pickWinner() public {
-
-    }
-
 }
