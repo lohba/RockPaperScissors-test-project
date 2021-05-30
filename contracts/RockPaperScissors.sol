85db2ba995e66409b3cb8d32162da7d531c88cc7
Luke Ohba <lukeohba@gmail.com>
2021-05-30 13:54:11 -0700

    hash

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -18,20 +18,21 @@ contract RockPaperScissors  is Ownable{
     struct Round {
          address player_1;
          address player_2;
-         bytes32 payer_1_move_hash;
-         bytes32 payer_2_move_hash;
+         bytes32 player_1_move_hash;
+         bytes32 player_2_move_hash;
          mapping(address => Move) moves;
      }
      
      struct Move {
          bytes hashMove;
-         uint256 finalMove;
+         Option finalMove;
      }
     
     
     //maps round ids to user moves. 
     mapping(uint256 => uint8) private firstChoice;
     mapping(uint256 => address) private gameStarter;
+    
     mapping(uint256 => Round) internal roundData;
     
     //0 = ROCK 1 = PAPER 2 = SCISSORS
@@ -51,11 +52,28 @@ contract RockPaperScissors  is Ownable{
         round.player_1 = msg.sender;
     }
     
-    //Make first move with id and move
-    function move(uint256 _roundId, uint256 _move) external{
+    //Make first move with id and hashed move
+    function move(uint256 _roundId, bytes32 _hashedMove) external{
         validPlayer(_roundId, msg.sender);
-        Move storage move = roundData[_roundId].moves[msg.sender];
-        move.finalMove = _move;
+        Round storage round = roundData[_roundId];
+        
+        if(msg.sender == round.player_1){
+            round.player_1_move_hash = _hashedMove;
+        } else {
+            round.player_2_move_hash = _hashedMove;
+        }
+    }
+    
+    // Reveal hashed moved
+    function reveal(uint256 _roundId, Option _option,  uint256 _salt) external {
+        require(keccak256(abi.encodePacked(_option, _salt)) == getCommit(_roundId), "incorrect hash");
+        require(_option == Option.ROCK || _option == Option.PAPER || _option == Option.SCISSORS);
+        
+        if(msg.sender == roundData[_roundId].player_1){
+            roundData[_roundId].moves[roundData[_roundId].player_1].finalMove = _option;
+        } else {
+            roundData[_roundId].moves[roundData[_roundId].player_2].finalMove = _option;
+        }
     }
     
     // Ensure valid player has joined the game
@@ -64,9 +82,17 @@ contract RockPaperScissors  is Ownable{
         require(round.player_1 == _player || round.player_2 == _player, "not valid player");
     }
     
-    function joinGame() public onlyOwner {
-        
+    // Create hash for a move
+    function createHashMove(Option _option, uint256 salt) public view returns(bytes32){
+        return keccak256(abi.encodePacked(_option, salt));
     }
+    
+    // Retrieve hash for a move
+    function getCommit(uint id) public view returns(bytes32){
+        return msg.sender == roundData[id].player_1 ? roundData[id].player_1_move_hash : roundData[id].player_2_move_hash;
+    }
+    
+    //
     
     
 
